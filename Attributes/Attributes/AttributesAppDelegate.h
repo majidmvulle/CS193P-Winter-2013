63718144e931b022242915e9fea9375737736ba8
majidmvulle <majid@majidmvulle.com>
2013-09-03 20:54:37 +0400

    Attributes
    Attributed String Class Demo

---
+++ Attributes/Attributes/AttributesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AttributesAppDelegate.h
+//  Attributes
+//
+//  Created by Majid Mvulle on 7/10/13.
+//  Copyright (c) 2013 Majid Mvulle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Attributes/Attributes/AttributesViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributesViewController.h
+//  Attributes
+//
+//  Created by Majid Mvulle on 7/10/13.
+//  Copyright (c) 2013 Majid Mvulle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributesViewController : UIViewController
+
+@end
